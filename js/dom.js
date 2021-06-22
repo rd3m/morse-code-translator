93d5907df69026d7d4988858a10cd816b150702d
rileydem <76932130+rileydem@users.noreply.github.com>
2021-06-22 15:51:40 +1000

    modular code, separating dom interactions from translation

--- js/dom.js
+++ js/dom.js
@@ -1,6 +1,14 @@
 import { toEnglish, toMorse } from "./translate.js";
 
-console.log(toMorse("hello I am riley"));
-console.log(
-    toEnglish(".... . .-.. .-.. ---    ..    .- --    .-. .. .-.. . -.--")
-);
+const translateBox = document.getElementById("translateBox");
+const outputBox = document.getElementById("outputBox");
+const translateButton = document.getElementById("translateButton");
+const toValue = document.getElementById("toValue");
+
+const runTranslation = () => {
+    toValue.value === "toMorse"
+        ? (outputBox.value = toMorse(translateBox.value.trim()))
+        : (outputBox.value = toEnglish(translateBox.value.trim()));
+};
+
+translateButton.addEventListener("click", runTranslation);
